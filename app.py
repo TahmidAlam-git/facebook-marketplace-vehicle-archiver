42733015a7dff251f6c86ea7329a1b47d0eea878
Ta-meed <34761115+Ta-meed@users.noreply.github.com>
2024-04-06 15:37:01 -0400

    add api endpoint for getting listings

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import yaml
 import json
 import os
 from bs4 import BeautifulSoup
+from fastapi import FastAPI
+import uvicorn
+import re
+
+app = FastAPI()
 
 BASE_URL = 'https://www.facebook.com'
 settings_file_name = 'settings.yml'
@@ -59,7 +64,20 @@ def get_matching_posts(page):
 
     return parsed
 
-def main():
+def pre_filter(posts):
+    for post in posts:
+        price = int(re.sub("[^0-9]", "", post['price']))
+        if price == '' or price <= 1234 or price >= 100000:
+            post['real'] = False
+        else:
+            post['real'] = True
+
+@app.get('/')
+def root():
+    return {'message': 'temporary root message'}
+
+@app.get("/scrape")
+def get_basic_listings():
     with sync_playwright() as pw:
         browser = pw.chromium.launch(headless=False)
         page = browser.new_page()
@@ -68,22 +86,30 @@ def main():
         posts = []
         for location in settings['locations']:
             for status in ['', 'availability=out of stock&']: # go through sold and non sold items
-
+                
+                # Go to the search page
                 page.goto(f"{BASE_URL}/marketplace/{location}/search?{status}daysSinceListed={settings['days-since-listed']}&sortBy={settings['sort-by']}&query={settings['vehicle']}&exact=false&radius=805")
                 page.wait_for_load_state("networkidle")
                 page.wait_for_timeout(5000)
 
+                # Load all the results
                 scroll_to_bottom(page)
                 page.wait_for_load_state("networkidle")
 
+                # Get the results
                 posts.extend(get_matching_posts(page))
+        pre_filter(posts)
 
         # print results
         for x, post in enumerate(posts):
             print(x, post)
         print('count', len(posts))
 
-        page.wait_for_timeout(60000)
+        return posts
 
 if __name__ == '__main__':
-    main()
+    uvicorn.run(
+        'app:app',
+        host='127.0.0.1',
+        port=8000
+    )
